7671d718058088284530609f37bf4fdb44a86e55
Jorge Bernal <jbernal@warp.es>
2009-01-14 19:42:42 +0100

    Enable binary log on master server

--- scripts/configure-replication-master.sh
+++ scripts/configure-replication-master.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+echo > /etc/my.cnf <<EOF
+[mysqld]
+log-bin=mysql-bin
+EOF
+invoke-rc.d mysql restart
 mysql -e "GRANT REPLICATION SLAVE to 'slave'@'192.168.2.2' IDENTIFIED BY 'slavepass'"
